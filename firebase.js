8ba43c12b0dcf17dd75fbc7ba25c49b95a19b837
prlusterio <pearlrosell.lusterio@gmail.com>
2023-06-03 16:17:16 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,16 +3,17 @@ import { getDatabase } from "firebase/database";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDG12zk3NnHzbLZU6K0ZE-30iEPAhIieX0",
-  authDomain: "hello-firebase-bf77e.firebaseapp.com",
-  databaseURL: "https://fir-crud-384ce-default-rtdb.firebaseio.com/",
-  projectId: "hello-firebase-bf77e",
-  storageBucket: "hello-firebase-bf77e.appspot.com",
-  messagingSenderId: "646208853359",
-  appId: "1:646208853359:web:3d964a7d50262e6156849a"
+  apiKey: "AIzaSyCHtZZC4EtjOYFoXOgwM7iNjwRjFeUug6w",
+  authDomain: "fir-crudapp-65207.firebaseapp.com",
+  databaseURL: "https://fir-crudapp-65207-default-rtdb.firebaseio.com/",
+  projectId: "fir-crudapp-65207",
+  storageBucket: "fir-crudapp-65207.appspot.com",
+  messagingSenderId: "476208411923",
+  appId: "1:476208411923:web:8db5af6116d52109e2d6e6"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
 export default db;
+
